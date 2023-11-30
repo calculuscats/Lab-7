b23a04dede19f282b62bdd3f805d315778ba10e2
calculuscats <151495232+calculuscats@users.noreply.github.com>
2023-11-30 14:31:35 -0800

    Add files via upload

--- FileIOExample.java
+++ FileIOExample.java
@@ -1,24 +1,76 @@
 import java.util.Scanner;
-import java.nio.file.Paths;
+import java.io.File;
 
 class FileIOExample {
 
     public static void main(String[] args) {
     
+    //Quantity, UnitPrice, OrderID, ProductID, Discount
+    
+    int[] quantity = new int[0];
+    double[] unitPrice = new double[1];
+    int[] orderID = new int[2];
+    int[] productID = new int[3];
+    double[] Disount = new double[4];
+   
+   
         try{
            File readFile = new File("OrderDetails.txt");
            Scanner in = new Scanner(readFile);
            
-           while (in.hasNext()){
-               System.out.println(in.nextLine());
+           while(in.hasNext()){
+             String data = in.nextLine();
+             
+             String[] arr = data.split(",",5);
+             
+           //  arr[0]=
+             
+            
+             //System.out.println(data);
+         
+             
            }
            
            in.close();
-     
-        } catch (Exeption e){
+           
+          } catch (Exception e){
             System.out.println("Error: Could not open file.");
-            
 
+          
+         }
+         
+     }
+}
+
+          
+           //keep doing while exists
+           /*for (int i = 0; i < 2820; i++) {//stores up to 2819 index
+          String str = in.nextLine(); //set the string str to next line
+          System.out.println(str);/*
+          
+           }
+           
+           //System.out.println("5");
+            
+         
+          
+         in.close(); 
+          
+     
+       } catch (Exception e){
+            //System.out.println("Error: Could not open file.");
+
+         }
+         
+           }
+           
+           //System.out.println("5");
+         System.out.println("5");
+
+         
+  //String[] str = new String [5];
+
+   
 }
 
 
@@ -26,7 +78,15 @@ class FileIOExample {
 
 
 
-/*//We create a scanner for reading file
+
+
+
+
+
+
+
+
+/*We create a scanner for reading file
 try(Scanner scanner = new Scanner(Paths.get("OrderDetails.txt"))) {
 
      //we read the file until all lines have been read
@@ -39,11 +99,9 @@ try(Scanner scanner = new Scanner(Paths.get("OrderDetails.txt"))) {
         
      }
 } catch (Exeption e) {
-    System.out.println("Error: " + e.getMessage());/*
+    System.out.println("Error: " + e.getMessage());*/
     
-        
-     
-     //array used with only classes;
-     
-     
-     
+
+
+
+
